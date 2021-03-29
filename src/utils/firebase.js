6eb00bc38e8ee8c8ccca4028b180eb0830655e79
Jose Alejandro Jimenez Rosa <jose.jimenez@inabima.gob.do>
2021-03-29 15:15:38 -0400

    Conectando React-native con Firebase

---
+++ src/utils/firebase.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCpwQALpCzYHYDu6wBMnm9gCtIDGX5osM4",
+    authDomain: "excellent-camp-300113.firebaseapp.com",
+    projectId: "excellent-camp-300113",
+    storageBucket: "excellent-camp-300113.appspot.com",
+    messagingSenderId: "581459071899",
+    appId: "1:581459071899:web:6c6456a17223c0d43d473e",
+    measurementId: "G-TD4W05ZF5M"
+  };
