9ed4a13fd85d428cc9bdcdbcdfa156e850eebdb3
Jose Alejandro Jimenez Rosa <jose.jimenez@inabima.gob.do>
2021-03-30 12:03:01 -0400

    Auth Screen

--- App.js
+++ App.js
@@ -1,26 +1,43 @@
 import React, { useEffect, useState } from 'react';
-import { SafeAreaView, Text, View } from 'react-native';
+import {StyleSheet, StatusBar, SafeAreaView, Text, View } from 'react-native';
+import Auth from './src/components/Auth';
 import firebase from './src/utils/firebase';
 import 'firebase/auth';
 
 
 
-export default function App(){
+export default function App() {
   const [user, setUser] = useState(undefined);
+
+
+  // firebase.auth().signOut();
 
   useEffect(() => {
     firebase.auth().onAuthStateChanged((response) => {
       setUser(response);
+      console.log(response);
     });
   }, []);
 
-  if(user === undefined){
+  if (user === undefined){
     return null;
   }
 
   return (
-    <SafeAreaView>
-        { user ? <Text>Estas Logueado</Text> : <Text>No estas Logeado </Text>}
+    <>
+    <StatusBar  barStyle='light-content' />
+    <SafeAreaView style={styles.background}>
+        { user ? <Text>Estas Logueado</Text> : <Auth/>}
     </SafeAreaView>
+    </>
   )
 }
+
+
+const styles = StyleSheet.create({
+  background: {
+    backgroundColor: "#15212b" ,
+    height: "100%",
+
+  }
+})
