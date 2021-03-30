8132fa797cfd8ac87d0f66e8d798ffd6ccd252c0
Jose Alejandro Jimenez Rosa <ja.jimenezrosa@gmail.com>
2021-03-30 15:05:54 -0400

    Formulario de Registro de Usuarios

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -22,5 +22,6 @@ const styles = StyleSheet.create({
     btnText: {
         color: "#FFF",
         fontSize: 20,
+        
     },
 })
--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -1,16 +1,46 @@
 import React from 'react'
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity, TextInput  } from 'react-native'
 
 export default function RegisterForm(props) {
     const {changeForm} = props;
 
+    const  register = () => {
+        console.log(register);
+    }
+
+
     return (
-        <View>
-            <Text>RegisterForm</Text>
-            <TouchableOpacity>
-                <Text style={styles.btnText} onPress={changeForm} >Inicia Sesion</Text>
+        <>
+            <TextInput 
+                style={styles.input}
+                placeholder="Correo electronico"
+                placeholderTextColor="#969696"
+            />
+            <TextInput 
+                style={styles.input}
+                placeholder="Contrasena"
+                placeholderTextColor="#969696"
+                secureTextEntry={true}
+                
+            />
+            <TextInput 
+                style={styles.input}
+                placeholder="Repetir Contrasena"
+                placeholderTextColor="#969696"
+                secureTextEntry={true}
+            />
+            <TouchableOpacity onPress={register} >
+                <Text style={styles.btnText}  >Registrarse</Text>
             </TouchableOpacity>
-        </View>
+
+            <View style={styles.login}>
+            <TouchableOpacity onPress={changeForm} >
+                <Text style={styles.btnText}  >Inicia Sesion</Text>
+            </TouchableOpacity>
+            </View>
+
+
+        </>
     )
 }
 
@@ -18,5 +48,22 @@ const styles = StyleSheet.create({
     btnText: {
         color: "#FFF",
         fontSize: 20,
+    },
+    input: {
+        height: 45,
+        color: "#FFF",
+        width: "80%",
+        marginBottom: 25,
+        backgroundColor: "#1e3040",
+        paddingHorizontal: 20, 
+        borderRadius: 50,
+        fontSize: 18,
+        borderWidth: 2,
+        borderColor: "#1e3040",
+    },
+    login: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        marginBottom: 15,
     },
 })
